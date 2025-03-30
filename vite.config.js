4255908703887a55fb88c2ac079c9197d8c311e5
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-30 06:28:58 +0600

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/eid-greetings-choto-via/",
+  base: "/eid-greetings-mezo-via/",
   plugins: [react(), tailwindcss()],
 })
